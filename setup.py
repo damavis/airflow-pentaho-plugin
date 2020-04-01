ce494428b18db91300808f612656ebdbfc0c7903
Cristòfol Torrens <piffall@gmail.com>
2020-04-01 15:57:46 +0200

    Adding LICENSE and long description in Markdown.

--- setup.py
+++ setup.py
@@ -16,22 +16,31 @@
 
 from setuptools import find_packages, setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="airflow-pentaho-plugin",
     version_format='{tag}.post{commitcount}',
+    license='Apache 2.0',
     setup_requires=['setuptools-git-version'],
     author='Damavis',
     author_email='info@damavis.com',
-    long_description='Pentaho pan and kitchen plugin for Apache Airflow',
-    url='https://damavis.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/damavis/airflow-pentaho-plugin',
     python_requires='>=3.6',
     test_suite='nose.collector',
     zip_safe=False,
     include_package_data=True,
     packages=find_packages('.', exclude=['tests', 'tests.*']),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: Unix'
+        'Environment :: Plugins',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3'
     ],
     install_requires=[
       "xmltodict >= 0.10.0"
