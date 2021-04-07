fb2fdee09d6c7512a429a1247c19814825570c10
Cristòfol Torrens <piffall@gmail.com>
2021-04-07 18:21:41 +0200

    Fixing classifiers

--- setup.py
+++ setup.py
@@ -44,9 +44,9 @@ setup(
         'Operating System :: Unix',
         'Operating System :: MacOS',
         'Operating System :: Microsoft :: MacOS',
-        'Programming Language :: Python :: 3.6'
-        'Programming Language :: Python :: 3.7'
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9'
     ],
     test_requires=[
